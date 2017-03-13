42531fddc0c4baac03a97b5c6a732d0f88efc909
KimDohyeun <ehgus-0530@hanmail.net>
2017-03-13 10:26:52 +0900

    p

--- resource/Host.py
+++ resource/Host.py
@@ -3,6 +3,7 @@ import cpu
 
 class Host:
 
+    Host_NUM = 0
     def __init__(self,ram,ID):
         self.CPU = []
         self.GPU = []
@@ -10,11 +11,26 @@ class Host:
 	self.Ram =ram
 	self.Remaining_Ram = ram
 	self.Host_ID = ID
+	
+    def add_cpu(self,MIPS,core_num):
 
-    def add_cpu(self,cpu):
+	self.CPU.append(cpu(MIPS,core_num))
 
-	self.CPU.append(cpu)
+    def add_gpu(self,gflops,memory,bandwidth,sm):
 
-    def add_gpu(self,gpu):
+	self.GPU.append(gpu(gflops,memory,bandwidth,sm))
+# delete cpu, gpu
+# allocate host resource to real vm instance
+# deallocate host resource to vm instance
+# ram memory allocation map with vm 
+# cpu gpu ->record which vm use resource host -> need to record this data to vm instance
 
-	self.GPU.append(gpu)
+
+
+
+
+
+class vm_resource_map:
+
+    def __init__(self, vmid):
+	vm_id = vmid
