46615b041bf109ea4ae7fec78b3bd570ffbab832
KimDohyeun <ehgus-0530@hanmail.net>
2017-03-14 20:43:33 +0900

    dd

--- resource/Host.py
+++ resource/Host.py
@@ -4,13 +4,15 @@ import cpu
 class Host:
 
     Host_NUM = 0
-    def __init__(self,ram,ID):
+    def __init__(self,ram):
         self.CPU = []
         self.GPU = []
 	self.switch = None
 	self.Ram =ram
 	self.Remaining_Ram = ram
-	self.Host_ID = ID
+	self.Host_ID = Host.Host_NUM
+	Host.Host_NUM = Host.Host_NUM + 1
+	self.vm_list = []
 	
     def add_cpu(self,MIPS,core_num):
 
@@ -24,6 +26,15 @@ class Host:
 # deallocate host resource to vm instance
 # ram memory allocation map with vm 
 # cpu gpu ->record which vm use resource host -> need to record this data to vm instance
+
+    def get_core(self,vm_id,num):
+
+	
+    def get_gpu(self,vm_id,num,mem):
+
+    def release_core(self,vm_id,num):
+	
+    def release_gpu(self,vm_id,num,mem):
 
 
 
---
+++ vm/vm.py
@@ -0,0 +1,26 @@
+class vm:
+
+    def __init__(self,vm_id):
+
+	self.hostlist = []
+	self.workload = []
+	self.Workload_handler = None
+	self.core_num = 0
+	self.sm_num = 0
+	self.gpu_mem = 0
+	self.mem = 0
+	self.ram = 0
+	self.gflops = 0
+	self.mips = 0
+	self.vm_id = vm_id
+	self.state = -1
+
+    def allocate_core(self, Host, num):
+	
+    def allocate_mem(self,Host , num):
+
+    
+
+    def process_vm():
+
+	return 0
