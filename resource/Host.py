15f3054a6f74c6d491ec463ea0318d144d4904b6
KimDohyeun <ehgus-0530@hanmail.net>
2017-03-15 20:39:56 +0900

    dd

--- resource/Host.py
+++ resource/Host.py
@@ -13,14 +13,21 @@ class Host:
 	self.Host_ID = Host.Host_NUM
 	Host.Host_NUM = Host.Host_NUM + 1
 	self.vm_list = []
-	
+	self.available_core = 0
+	self.available_sm = 0
+	self.available_gpu_mem = 0
     def add_cpu(self,MIPS,core_num):
 
 	self.CPU.append(cpu(MIPS,core_num))
+	self.available_core = self.available_core + core_num
 
     def add_gpu(self,gflops,memory,bandwidth,sm):
 
 	self.GPU.append(gpu(gflops,memory,bandwidth,sm))
+	self.available_gpu_mem = self.available_gpu_mem + memory
+	self.available_sm = self.available_sm + sm
+
+
 # delete cpu, gpu
 # allocate host resource to real vm instance
 # deallocate host resource to vm instance
@@ -28,8 +35,27 @@ class Host:
 # cpu gpu ->record which vm use resource host -> need to record this data to vm instance
 
     def get_core(self,vm_id,num):
+	mips_tmp = 0
+	if self.available_core <num :
+	    
+	    return None
+	else :
+	    for k in self.CPU:
+		if k.Available_core >0 :
+		    if k.Available_core < tmp:
+			tmp = tmp - k.Available_core
+			self.available_core= self.available_core - k.Available_core
+			k.allocate_core(k.Available_core,vm_id)
+			cpu_list.append(k)
+		    else :
+			k.allocate_core(tmp,vm_id)
+			self.available_core = self.availalbe_core - tmp
+			tmp = 0
+			cpu_list.append(k)
+		if tmp == 0 :
+		    break
 
-	
+	    return cpu_list
     def get_gpu(self,vm_id,num,mem):
 
     def release_core(self,vm_id,num):
@@ -39,9 +65,3 @@ class Host:
 
 
 
-
-
-class vm_resource_map:
-
-    def __init__(self, vmid):
-	vm_id = vmid
--- vm/vm.py
+++ vm/vm.py
@@ -2,7 +2,8 @@ class vm:
 
     def __init__(self,vm_id):
 
-	self.hostlist = []
+	self.host_list = []
+	self.resource_list = []
 	self.workload = []
 	self.Workload_handler = None
 	self.core_num = 0
@@ -15,12 +16,33 @@ class vm:
 	self.vm_id = vm_id
 	self.state = -1
 
+    def allocate_workload(self, workload):
+	self.workload.append(workload)
+
+
     def allocate_core(self, Host, num):
-	
+	if Host.get_core(num) is True:
+	    tmp = True
+	    for k in self.hostlist:
+		if k.Host_ID == Host.Host_ID
+		    tmp = False
+	    if tmp :
+		self.hostlist.append(Host)
+	    self.core_num = self.core_num + num
+	    self.mips = self.mips + Host
+
     def allocate_mem(self,Host , num):
 
-    
+    def allocate_gpu(self,Host,num,mem):  
 
     def process_vm():
 
 	return 0
+
+class vm_host_resource:
+
+    def __init__(self, core,ram,sm,gpu_ram):
+	self.core = core
+	self.sm = sm
+	self.ram = ram
+	self.gpu_ram = ram
